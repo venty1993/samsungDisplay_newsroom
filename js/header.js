26d5b47b1ae0ca451405199a8343db68f86f2e97
venty1993 <75829033+venty1993@users.noreply.github.com>
2022-11-24 12:54:07 +0900

    index에 병합

--- js/header.js
+++ js/header.js
@@ -2,9 +2,9 @@ const headerNav = document.querySelector('.header-nav');
 const header = document.querySelector('header');
 
 headerNav.addEventListener('mouseenter', () =>{
-    header.classList.add('active');
+    header.classList.add('header-active');
 })
 
 header.addEventListener('mouseleave', () => {
-    header.classList.remove('active');
+    header.classList.remove('header-active');
 })
